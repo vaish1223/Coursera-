6542041c8d18de0dd873e136a9ea206c94ff6fb6
vaish1223 <93441773+vaish1223@users.noreply.github.com>
2022-06-11 13:29:16 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+from machinetranslation import translator
+from flask import Flask, render_template, request
+import json
+
+app = Flask("Web Translator")
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.english_to_french(text_to_translate)
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    text_to_translate = request.args.get('textToTranslate')
+    return translator.french_to_english(text_to_translate)
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
